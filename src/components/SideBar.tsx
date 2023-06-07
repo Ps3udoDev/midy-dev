ca9215376f5d4860a2c14c0954c88d709f40e0fe
Pseudo <v.pseudo.developer@gmail.com>
2023-06-07 14:52:40 -0500

    feat: sidebar and header mockup

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -6,6 +6,7 @@ import { HiHome } from 'react-icons/hi';
 import { BiSearch } from 'react-icons/bi';
 import Box from './Box';
 import SideBarItem from './SideBarItem';
+import Library from './Library';
 
 
 interface SideBarProps {
@@ -32,7 +33,7 @@ const SideBar: React.FC<SideBarProps> = ({ children }) => {
 
   return (
     <div className='flex h-full'>
-      <div className='hidden md:flex flex-col gap-y-2 h-full w-[300px] p-2 bg-bg-color'>
+      <div className='hidden md:flex flex-col gap-y-2 h-full w-[300px] p-2 bg-black'>
       <Box>
         <div className='flex flex-col gap-y-4 px-5 py-4'>
           {routes.map((item)=> (
@@ -41,7 +42,7 @@ const SideBar: React.FC<SideBarProps> = ({ children }) => {
         </div>
       </Box>
       <Box className='overflow-y-auto h-full'>
-        Song Library
+       <Library />
       </Box>
       </div>
       <main className='h-full flex-1 overflow-y-auto py-2'>
