4f63526529bb27211233adb4a26d3ae8ae18e19f
Pseudo <v.pseudo.developer@gmail.com>
2023-06-14 13:17:22 -0500

    feat: upload Modal and implement the funtionality

--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -1,11 +1,20 @@
 'use client'
 import { TbPlaylist } from 'react-icons/tb'
 import { AiOutlinePlus } from 'react-icons/ai'
+import useAuthModal from '../../hooks/useAuthModal'
+import { useUser } from '../../hooks/useUser'
+import useUploadModal from '../../hooks/useUploadModal'
 
 const Library = () => {
-  const onClick = () => {
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
 
-  }
+  const onClick = () => {
+    if (!user) return authModal.onOpen();
+    
+    return uploadModal.onOpen()
+  };
 
   return (
     <div className='flex flex-col'>
@@ -16,7 +25,11 @@ const Library = () => {
             Your Library
           </p>
         </div>
-        <AiOutlinePlus onClick={onClick} size={20} className='text-neutral-400 cursor-pointer hover:text-white transition'/>
+        <AiOutlinePlus
+          onClick={onClick}
+          size={20}
+          className='text-neutral-400 cursor-pointer hover:text-white transition'
+        />
       </div>
       <div className='flex flex-col gap-y-2 mt-4 px-3'>
         List Of Songs!
